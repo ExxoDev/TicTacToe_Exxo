08ef42fd60ac3c268ec6ce353ee68b4cb4e72c91
stefang37 <stefan.gaier@web.de>
2013-05-13 14:42:06 +0200

    Update Rechner.java

--- Rechner.java
+++ Rechner.java
@@ -1,44 +1,71 @@
-package Methoden_test;
+package methoden;
 
-public class rechnung {
+public class hausaufgabe_13_05 {
 
-  /**
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		int x;
-		int y;
-		String zeichen = "+";
-		
-		int ergebnis = rechne(1,2, "+" );
-		System.out.println(ergebnis);
 	}
-
 	
+	// 1.) Methode, die 2 Zahlen addiert
+	public static float addiere(double a, double b){
+		float ergebnis = (float) (a + b);
+		return ergebnis;
+	}
 	
-	private static int rechne(int a, int b, String z){
-		int ergebnis = 0;
+	// 2.) Methode, die 2 Zahlen subtrahiert
+	public static float subtrahiere(double a, double b){
+		float ergebnis = (float) (a - b);
+		return ergebnis;
+	}
+	
+	// 3.) Methode, die 2 Zahlen multipliziert
+	public static float multipliziere(double a, double b){
+		float ergebnis = (float) (a * b);
+		return ergebnis;
+	}
+	
+	// 4.) Methode, die 2 Zahlen dividiert
+	public static float dividiere(double a, double b){
+		float ergebnis = (float) (a / b);
+		return ergebnis;
+	}
+	
+	// 5.) Optionen der Methode einlesen, berechnen und ausgeben
+	private static float rechne(double a, String z, double b){
+		float ergebnis = 0;
 		
 		switch(z){
 		case "+" :
-			ergebnis = a + b;
+			ergebnis = (float) (a + b);
 			break;
 		case "-" :
-			ergebnis = a - b;
+			ergebnis = (float) (a - b);
 			break;
 		case "/":
-			ergebnis = a / b;
+			ergebnis = (float) (a / b);
 			break;
 		case "*":
-			ergebnis = a * b;
+			ergebnis = (float) (a * b);
 			break;
 		default : 
 			System.out.println("Die Eingabe des Rechenzeichens konnte nicht erkannt werden");
 		}
 		
+		// Ausgabe auf der Konsole mit entsprechender Rechenfolge
+		System.out.println(a + " " + z + " " + b  + " = " + ergebnis);
+		
+		// Ergebnis wird zurückgegeben
 		return ergebnis;
+		}
 	
+	// 6. Berechnung des Volumens eines Kegels
+	private static float kegel(double radius, double hoehe){
+		double pi = 3.14159265358979323846264338327950288419716939937510582097494459;
+		float ergebnis = (float) ( 0.33333334 * pi *  (radius * radius)  * hoehe   );
+		System.out.println("Das Volumen eines Kegels mit dem Radius " + radius + " und der Höhe " + hoehe + " beträgt " + ergebnis);
+		return ergebnis;
 	}
 
 }
